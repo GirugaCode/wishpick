d6db3917c883c3c7e6f6a9ca1403b6499f2c5520
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-06-25 21:35:45 -0700

    [init]

---
+++ wishpick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wishpick
+//
+//  Created by Ryan Nguyen on 6/25/19.
+//  Copyright © 2019 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

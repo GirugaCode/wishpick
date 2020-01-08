7e18310e618d1b611ae5aab5d78fa1bf04946b0f
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2020-01-08 11:14:47 -0800

    [add] photos will save to fire base storage

--- wishpick/Controllers/SharePhotoController.swift
+++ wishpick/Controllers/SharePhotoController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Danh Phu Nguyen. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 class SharePhotoController: UIViewController {
@@ -86,7 +87,46 @@ class SharePhotoController: UIViewController {
     }
     
     @objc func handleShare() {
-        print("Handle Share")
+        guard let image = selectedImage else { return }
+        
+        guard let uploadData = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child("posts").child(filename)
+        storageRef.putData(uploadData, metadata: nil) { (metadata, err) in
+            if let err = err {
+                print("Failed to upload image", err)
+                return
+            }
+            storageRef.downloadURL { (imageUrl, error) in
+                if error != nil {
+                    print("Failed to download URL:", error as Any)
+                }
+                print("Successfully uploaded image URL", imageUrl as Any)
+                
+                guard let imageUrlString = imageUrl?.absoluteString else { return }
+                self.saveToDBwithURL(imageUrl: imageUrlString)
+            }
+        }
+    }
+    
+    fileprivate func saveToDBwithURL(imageUrl: String) {
+        guard let postImage = selectedImage else { return }
+        guard let itemInfo = descriptionView.text else { return }
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let userPostRef = Database.database().reference().child("posts").child(uid)
+        let ref = userPostRef.childByAutoId()
+        
+        let values = ["imageUrl": imageUrl, "itemInfo": itemInfo, "imageWidth": postImage.size.width, "imageHeight": postImage.size.height, "creationDate": Date().timeIntervalSince1970] as [String : Any]
+        
+        ref.updateChildValues(values) { (err, ref) in
+            if let err = err {
+                print("Failed to save post to DB", err)
+                return
+            }
+            print("Successfully saved post to DB", ref)
+        }
     }
     
     /// Hides the status bar on top of device
